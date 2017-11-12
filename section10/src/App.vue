9ddb4a424ad64c66d7b5555b75fab7ae3821bc53
osadnriets@gmail.com <gjx2890-12345>
2017-11-12 22:23:49 +0200

    selection 10

---
+++ section10/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="container">
+        <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
+        <app-new-quote @quoteAdded="newQuote"></app-new-quote>
+        <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <div class="alert alert-info"> Into: Click on Quote to delete if!</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Header from './components/Header.vue';
+    import QuoteGrid from './components/QuoteGrid.vue';
+    import NewQuote from './components/NewQuote.vue';
+
+    export default {
+        data: function () {
+            return{
+                quotes: [
+                    'Just a Quote to see something'
+                ],
+                maxQuotes: 10
+            }
+        },
+        methods: {
+            newQuote(quote){
+                if(this.quotes.length >= this.maxQuotes){
+                    return alert('Please delete Quotes first!');
+                }
+                this.quotes.push(quote);
+            },
+            deleteQuote(index) {
+                this.quotes.splice(index, 1);
+            }
+        },
+        components: {
+            appHeader: Header,
+            appQuoteGrid: QuoteGrid,
+            appNewQuote: NewQuote
+        }
+    }
+</script>
+
+<style>
+</style>
